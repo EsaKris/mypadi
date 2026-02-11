3920416a70f5f189510220f2fc7bb75c094f6dfb
Esa Kris <135467359+EsaKris@users.noreply.github.com>
2026-02-11 12:16:29 +0000

    updated views and urls for landing, and added about contact pages:   landing/urls.py 	updated views and urls for landing, and added about contact pages:   landing/views.py 	updated views and urls for landing, and added about contact pages:   mypadi/settings.py 	updated views and urls for landing, and added about contact pages:   static/img/dr_shed.jpg 	updated views and urls for landing, and added about contact pages:   static/img/esakris.png 	updated views and urls for landing, and added about contact pages:   templates/base.html 	updated views and urls for landing, and added about contact pages:   templates/landing/about.html 	updated views and urls for landing, and added about contact pages:   templates/landing/contact.html 	updated views and urls for landing, and added about contact pages:   templates/landing/cookie.html 	updated views and urls for landing, and added about contact pages:   templates/landing/policy.html 	updated views and urls for landing, and added about contact pages:   templates/landing/terms.html

--- landing/urls.py
+++ landing/urls.py
@@ -9,4 +9,12 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('properties/', PropertyListView.as_view(), name='property_list'),
     path('properties/<slug:slug>/', PropertyDetailView.as_view(), name='property_detail'),
+        # Legal Pages
+    path('terms-of-service/', views.TermsOfServiceView.as_view(), name='terms'),
+    path('privacy-policy/', views.PrivacyPolicyView.as_view(), name='policy'),
+    path('cookie-policy/', views.CookiePolicyView.as_view(), name='cookie'),
+    
+    # Company Pages
+    path('about-us/', views.AboutUsView.as_view(), name='about'),
+    path('contact-us/', views.ContactView.as_view(), name='contact'),
 ]
